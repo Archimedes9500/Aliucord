c784fc3230d48bfcbb64bea4af7a33d38e48bacd
Archimedes9500 <82618548+Archimedes9500@users.noreply.github.com>
2025-09-25 22:06:04 +0200

    Update PluginDownloader.kt

--- Aliucord/src/main/java/com/aliucord/coreplugins/plugindownloader/PluginDownloader.kt
+++ Aliucord/src/main/java/com/aliucord/coreplugins/plugindownloader/PluginDownloader.kt
@@ -73,43 +73,15 @@ internal class PluginDownloader : CorePlugin(Manifest("PluginDownloader")) {
     }
 
     override fun start(context: Context) {
+        fun addPluginDownloadOptions("
+
         patcher.patch(
             WidgetChatListActions::class.java.getDeclaredMethod("configureUI", WidgetChatListActions.Model::class.java),
             Hook { (param, model: WidgetChatListActions.Model) ->
                 val actions = param.thisObject as WidgetChatListActions
-                val layout = (actions.requireView() as ViewGroup).getChildAt(0) as ViewGroup
-
-                if (layout.findViewById<View>(viewId) != null) return@Hook
-
                 val msg = model.message
-                val content = msg?.content ?: return@Hook
-                val me = StoreStream.getUsers().me
 
-                when (msg.channelId) {
-                    PLUGIN_LINKS_UPDATES_CHANNEL_ID, PLUGIN_DEVELOPMENT_CHANNEL_ID, if(msg.author.id === me.id) msg.channelId else -1 ->
-                        handlePluginZipMessage(msg, layout, actions)
-
-                    SUPPORT_CHANNEL_ID, PLUGIN_SUPPORT_CHANNEL_ID -> {
-                        val member = StoreStream.getGuilds().getMember(ALIUCORD_GUILD_ID, msg.author.id)
-                        val isTrusted = member?.roles?.any { it in arrayOf(SUPPORT_HELPER_ROLE_ID, PLUGIN_DEVELOPER_ROLE_ID) } ?: false
-
-                        if (isTrusted) handlePluginZipMessage(msg, layout, actions)
-                    }
-
-                    PLUGIN_LINKS_CHANNEL_ID -> {
-                        repoPattern.matcher(content).takeIf { it.find() }?.run {
-                            val author = group(1)!!
-                            val repo = group(2)!!
-
-                            addEntry(layout, "Open Plugin Downloader") {
-                                Utils.openPageWithProxy(it.context, Modal(author, repo))
-                                actions.dismiss()
-                            }
-                        }
-                    }
-
-                    else -> if(msg.author.id === me.id) handlePluginZipMessage(msg, layout, actions)
-                }
+                addPluginDownloadOptions(msg, actions:)
             }
         )
         //also for link context menu
@@ -136,57 +108,86 @@ internal class PluginDownloader : CorePlugin(Manifest("PluginDownloader")) {
             WidgetUrlActions::class.java.getDeclaredMethod("onViewCreated", View::class.java, android.os.Bundle::class.java),
             Hook { (param, view: View, bundle: android.os.Bundle) ->
                 val actions = param.thisObject as WidgetUrlActions
-                val layout = ((ReflectUtils.getField(actions, "binding\$delegate") as FragmentViewBindingDelegate<WidgetUrlActionsBinding>) //val layout = actions.`binding$delegate`
-                    .getValue(actions as Fragment, WidgetUrlActions.`$$delegatedProperties`[0]) as WidgetUrlActionsBinding
-                    ).getRoot() as ViewGroup
-                val url = WidgetUrlActions.`access$getUrl$p`(actions)
-    
-                if (layout.findViewById<View>(urlViewId) != null) return@Hook
-    
                 val msg = actions.getExt(fUrlSource2) as? Message?
-                val content = msg?.content ?: return@Hook
-                when (msg.channelId) {
-                    PLUGIN_LINKS_UPDATES_CHANNEL_ID, PLUGIN_DEVELOPMENT_CHANNEL_ID ->
-                        handlePluginZipUrl(url, layout, actions)
-    
-                    SUPPORT_CHANNEL_ID, PLUGIN_SUPPORT_CHANNEL_ID -> {
-                        val member = StoreStream.getGuilds().getMember(ALIUCORD_GUILD_ID, msg.author.id)
-                        val isTrusted = member?.roles?.any { it in arrayOf(SUPPORT_HELPER_ROLE_ID, PLUGIN_DEVELOPER_ROLE_ID) } ?: false
-    
-                        if (isTrusted) handlePluginZipUrl(url, layout, actions)
-                    }
-    
-                    PLUGIN_LINKS_CHANNEL_ID -> {
-                        repoPattern.matcher(url).takeIf { it.find() }?.run {
-                            val author = group(1)!!
-                            val repo = group(2)!!
-    
-                            addUrlEntry(layout, "Open Plugin Downloader") {
-                                Utils.openPageWithProxy(it.context, Modal(author, repo))
-                                actions.dismiss()
-                            }
-                        }
-                    }
-                }
+
+                addPluginDownloadOptions(msg, actions)
             }
         )
     }
 
     override fun stop(context: Context) {}
 
-    fun handlePluginZipUrl(str: String, layout: ViewGroup, actions: WidgetUrlActions) {
-        zipPattern.matcher(str).run {
-            while (find()) {
-                val author = group(1)!!
-                val repo = group(2)!!
-				val commit = group(3)!!
-                val name = group(4)!!
+    fun addPluginDownloadOptions(msg: Message, actions: AppBottomSheet) {
+        when(actions) {
+            is WidgetChatListActions -> {
+                val layout = (actions.requireView() as ViewGroup).getChildAt(0) as ViewGroup
+                val target = "dialog_chat_actions_edit"
+                val str = msg?.content ?: return
+ 
+                if (layout.findViewById<View>(viewId) != null) return
+            }
+
+            is WidgetUrlActions -> {
+                val layout = ((ReflectUtils.getField(actions, "binding\$delegate") as FragmentViewBindingDelegate<WidgetUrlActionsBinding>) //val layout = actions.`binding$delegate`
+                    .getValue(actions as Fragment, WidgetUrlActions.`$$delegatedProperties`[0]) as WidgetUrlActionsBinding
+                    ).getRoot() as ViewGroup
+                val target = "dialog_url_actions_copy"
+                val str = WidgetUrlActions.`access$getUrl$p`(actions)
+
+                if (layout.findViewById<View>(urlViewId) != null) return
+            }
+        }
+        val me = StoreStream.getUsers().me
+
+        if(msg.author.id == me.id){
+            handlePluginMessage(str, layout, actions)
+            handlePluginRepoMessage(str, layout, actions)
+            handlePluginAttachments(msg, layout, actions)
+        } else {
+            when (msg.channelId) {
+                PLUGIN_LINKS_UPDATES_CHANNEL_ID, PLUGIN_DEVELOPMENT_CHANNEL_ID -> {
+                    handlePluginMessage(str, layout, actions)
+                    handlePluginAttachments(msg, layout, actions)
+                }
+    
+                SUPPORT_CHANNEL_ID, PLUGIN_SUPPORT_CHANNEL_ID -> {
+                    val member = StoreStream.getGuilds().getMember(ALIUCORD_GUILD_ID, msg.author.id)
+                    val isTrusted = member?.roles?.any { it in arrayOf(SUPPORT_HELPER_ROLE_ID, PLUGIN_DEVELOPER_ROLE_ID) } ?: false
+    
+                    if (isTrusted) {
+                        handlePluginMessage(str, layout, actions)
+                        handlePluginAttachments(msg, layout, actions)
+                    }
+                }
+    
+                PLUGIN_LINKS_CHANNEL_ID -> {
+                    handlePluginMessage(str, layout, actions)
+                }
+            }
+        }
+    }
+
+    fun handlePluginRepoMessage(str: String, layout: ViewGroup, actions: AppBottomSheet) {
+        if (repoPattern.containsMatchIn(str)) {
+            val (author, repo) = repoPattern.find(str).destructured
+
+            addEntryBefore(layout, "Open Plugin Downloader", targetId) {
+                Utils.openPageWithProxy(it.context, Modal(author, repo))
+                actions.dismiss()
+            }
+        }
+    }
+
+    fun handlePluginMessage(str: String, layout: ViewGroup, actions: AppBottomSheet) {
+        if (zipPattern.containsMatchIn(str)) {
+            for (match in zipPattern.findAll(str)) {
+                val (author, repo, commit, name) = match.destructured
 
                 // Don't accidentally install core as a plugin
                 if (name == "Aliucord") continue
 
                 val plugin = PluginFile(name)
-                addUrlEntry(layout, "${if (plugin.isInstalled) "Reinstall" else "Install"} $name") {
+                addEntryBefore(layout, "${if (plugin.isInstalled) "Reinstall" else "Install"} $name", targetId) {
                     plugin.install("https://github.com/$author/$repo/raw/$commit/$name.zip")
                     actions.dismiss()
                 }
@@ -194,32 +195,14 @@ internal class PluginDownloader : CorePlugin(Manifest("PluginDownloader")) {
         }
     }
 
-    fun handlePluginZipMessage(msg: Message, layout: ViewGroup, actions: WidgetChatListActions) {
-        zipPattern.matcher(msg.content).run {
-            while (find()) {
-                val author = group(1)!!
-                val repo = group(2)!!
-				val commit = group(3)!!
-                val name = group(4)!!
-
-                // Don't accidentally install core as a plugin
-                if (name == "Aliucord") continue
-
-                val plugin = PluginFile(name)
-                addEntry(layout, "${if (plugin.isInstalled) "Reinstall" else "Install"} $name") {
-                    plugin.install("https://github.com/$author/$repo/raw/$commit/$name.zip")
-                    actions.dismiss()
-                }
-            }
-        }
-
+    fun handlePluginAttachments(msg: Message, layout: ViewGroup, actions: AppBottomSheet) {
         for (attachment in msg.attachments) {
             if (attachment.filename.run { !endsWith(".zip") || equals("Aliucord.zip") }) continue
 
             val name = attachment.filename.removeSuffix(".zip")
             val isInstalled = PluginManager.plugins.containsKey(name)
 
-            addEntry(layout, "${if (isInstalled) "Reinstall" else "Install"} $name") {
+            addEntryBefore(layout, "${if (isInstalled) "Reinstall" else "Install"} $name") {
                 PluginFile(name).install(
                     url = attachment.url,
                     callback = actions::dismiss,
@@ -228,29 +211,10 @@ internal class PluginDownloader : CorePlugin(Manifest("PluginDownloader")) {
         }
     }
 
-    fun addUrlEntry(layout: ViewGroup, text: String, onClick: View.OnClickListener) {
-        val copyView =
-            layout.findViewById<View>(Utils.getResId("dialog_url_actions_copy", "id")) ?: return
-        val idx = layout.indexOfChild(copyView)
-
-        TextView(layout.context, null, 0, R.i.UiKit_Settings_Item_Icon).run {
-            id = viewId
-            setText(text)
-            setOnClickListener(onClick)
-            ContextCompat.getDrawable(layout.context, R.e.ic_file_download_white_24dp)?.run {
-                mutate()
-                setTint(ColorCompat.getThemedColor(layout.context, R.b.colorInteractiveNormal))
-                setCompoundDrawablesRelativeWithIntrinsicBounds(this, null, null, null)
-            }
-
-            layout.addView(this, idx)
-        }
-    }
-
-    fun addEntry(layout: ViewGroup, text: String, onClick: View.OnClickListener) {
-        val replyView =
-            layout.findViewById<View>(Utils.getResId("dialog_chat_actions_edit", "id")) ?: return
-        val idx = layout.indexOfChild(replyView)
+    fun addEntryBefore(layout: ViewGroup, text: String, targetId: String, onClick: View.OnClickListener) {
+        val targetView =
+            layout.findViewById<View>(Utils.getResId(targetId, "id")) ?: return
+        val idx = layout.indexOfChild(targetView)
 
         TextView(layout.context, null, 0, R.i.UiKit_Settings_Item_Icon).run {
             id = viewId
