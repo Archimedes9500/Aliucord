a1048f20d5421c228ef7fe7744cdaf9e2e240630
Archimedes9500 <82618548+Archimedes9500@users.noreply.github.com>
2026-02-18 04:23:46 +0100

    Update Typealiases.kt

--- Aliucord/src/main/java/com/aliucord/Typealiases.kt
+++ Aliucord/src/main/java/com/aliucord/Typealiases.kt
@@ -1,7 +1,4 @@
 package com.aliucord
 
-import kotlin.collections.IntIterator
-import kotlin.ranges.IntProgressionIterator
-
 typealias IntIterator = d0.t.c0
 typealias IntProgressionIterator = d0.d0.b
