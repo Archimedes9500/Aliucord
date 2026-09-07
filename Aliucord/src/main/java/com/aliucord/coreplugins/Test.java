faf166c0996472f32febf9df2deab4b3194a5b92
Archimedes9500 <82618548+Archimedes9500@users.noreply.github.com>
2026-09-07 13:33:21 +0200

    Create Test.java

---
+++ Aliucord/src/main/java/com/aliucord/coreplugins/Test.java
@@ -0,0 +1,25 @@
+package com.aliucord.coreplugins;
+
+import com.aliucord.entities.CorePlugin;
+import com.aliucord.entities.Plugin.Manifest;
+import com.aliucord.api.SettingsAPI;
+import com.google.gson.reflect.TypeToken;
+import com.aliucord.Logger;
+
+class TestJava extends CorePlugin{	
+public TestJava(){
+	super(new Manifest("TestJava"));	
+};
+	var _settings = new SettingsAPI("Test");
+	var thing = _settings.getObject("thing", ArrayList<Balls>(), new TypeToken<List<Balls>>(){}.getType());
+	var logger = new Logger("TestJava");
+	logger.debug(thing.toArray(new Balls[0]).toString());
+	try{
+		thing = _settings.getObject("thing", ArrayList<Balls>());
+		for(Balls balls : thing){
+			var temp = balls.balls;
+		};
+	}catch(e: Throwable){
+		logger.error("Yop, it crashed", e);
+	}
+};
