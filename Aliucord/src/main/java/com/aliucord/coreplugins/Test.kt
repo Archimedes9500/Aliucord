4ef1caf2393e9cd2da93ed9409ebb8bd961ae757
Archimedes9500 <82618548+Archimedes9500@users.noreply.github.com>
2026-09-07 14:11:45 +0200

    Update Test.kt

--- Aliucord/src/main/java/com/aliucord/coreplugins/Test.kt
+++ Aliucord/src/main/java/com/aliucord/coreplugins/Test.kt
@@ -3,6 +3,7 @@ package com.aliucord.coreplugins;
 import com.aliucord.entities.CorePlugin;
 import com.aliucord.entities.Plugin.Manifest;
 import com.aliucord.api.SettingsAPI;
+import android.context.Context;
 import com.aliucord.api.getObject;
 import com.google.gson.reflect.TypeToken;
 import com.aliucord.Logger;
@@ -10,6 +11,8 @@ import com.aliucord.Logger;
 data class Balls(val balls: String);
 internal class TestKotlin: CorePlugin(Manifest("TestKotlin")){
 	val _settings = SettingsAPI("Test");
-	val thing = _settings.getObject("thing", ArrayList<Balls>());
-	Logger("TestKotlin").debug(thing.joinToString("\n"));
+    override fun start(context: Context){
+        val thing = _settings.getObject("thing", ArrayList<Balls>());
+        Logger("TestKotlin").debug(thing.joinToString("\n"));
+    };
 };
