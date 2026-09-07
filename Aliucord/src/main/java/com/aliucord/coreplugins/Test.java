70411c188f9fd4790216555b9032c7ae65d57011
Archimedes9500 <82618548+Archimedes9500@users.noreply.github.com>
2026-09-07 14:14:40 +0200

    Update Test.java

--- Aliucord/src/main/java/com/aliucord/coreplugins/Test.java
+++ Aliucord/src/main/java/com/aliucord/coreplugins/Test.java
@@ -3,23 +3,27 @@ package com.aliucord.coreplugins;
 import com.aliucord.entities.CorePlugin;
 import com.aliucord.entities.Plugin.Manifest;
 import com.aliucord.api.SettingsAPI;
+import android.content.Context;
 import com.google.gson.reflect.TypeToken;
 import com.aliucord.Logger;
 
-class TestJava extends CorePlugin{	
-public TestJava(){
-	super(new Manifest("TestJava"));	
-};
+class TestJava extends CorePlugin{
+    public TestJava(){
+        super(new Manifest("TestJava"));
+    };
 	var _settings = new SettingsAPI("Test");
-	var thing = _settings.getObject("thing", ArrayList<Balls>(), new TypeToken<List<Balls>>(){}.getType());
-	var logger = new Logger("TestJava");
-	logger.debug(thing.toArray(new Balls[0]).toString());
-	try{
-		thing = _settings.getObject("thing", ArrayList<Balls>());
-		for(Balls balls : thing){
-			var temp = balls.balls;
-		};
-	}catch(e: Throwable){
-		logger.error("Yop, it crashed", e);
-	}
+    
+    @Override void start(Context context){
+        var thing = _settings.getObject("thing", ArrayList<Balls>(), new TypeToken<List<Balls>>(){}.getType());
+        var logger = new Logger("TestJava");
+        logger.debug(thing.toArray(new Balls[0]).toString());
+        try{
+            thing = _settings.getObject("thing", ArrayList<Balls>());
+            for(Balls balls : thing){
+                var temp = balls.balls;
+            };
+        }catch(e: Throwable){
+            logger.error("Yop, it crashed", e);
+        };
+    };
 };
